51555d436f9dee5db4a9e30acf774a50a4233eb0
deepu-ak <deepakhah970@gmail.com>
2024-09-20 21:13:47 +0545

    Add files via upload

---
+++ cursor.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('JavaScript file loaded successfully!');
+    
+    function exampleFunction() {
+        console.log('This is an example function.');
+    }
+
+    exampleFunction();
+});
